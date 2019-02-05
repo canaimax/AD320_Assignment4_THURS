5e0a0777b434be99b732e8f5d24497cac0195f72
Sanford Winslow <sanfordwinslow@Sanfords-MacBook-Pro.local>
2019-02-04 20:52:09 -0800

    adding new code

---
+++ sanford-winslow.php
@@ -0,0 +1,17 @@
+<?php
+
+echo
+"<div display: inline-block'>"
+
+ require 'testData.php';
+  "<ul>";
+ foreach($dataToPrintToScreen as $key => $value) {
+   echo "<h3>$key</h3>";
+   echo "<li>$value</li>";
+ }
+  "</ul>";
+
+ "</div>";
+
+
+ ?>
